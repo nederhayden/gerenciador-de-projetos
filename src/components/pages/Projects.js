bdc7b19495cfe5a1ebabbad67432f45ed12c1778
Neder <nedersimoes@gmail.com>
2021-09-20 18:24:51 -0400

    system message

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,17 @@
+import { useLocation } from "react-router-dom";
+import Message from "../layout/Message";
+
 export default function Projects() {
-  return <h1>Projects</h1>;
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div>
+      <h1>Meus Projetos</h1>
+      {message && <Message type="success" msg={message} />}
+    </div>
+  );
 }

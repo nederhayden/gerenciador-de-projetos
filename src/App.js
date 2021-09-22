bad252b50cf34aa697bd9b66b7ef640290c8bf5d
Neder <nedersimoes@gmail.com>
2021-09-22 19:05:52 -0400

    rescuing projects from the database

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
-import NewProject from "./components/pages/NewProject";
 import Projects from "./components/pages/Projects";
+import NewProject from "./components/pages/NewProject";
 
 import Container from "./components/layout/Container";
 import Navbar from "./components/layout/Navbar";
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,31 @@
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
 
 import styles from "./Projects.module.css";
 
 export default function Projects() {
+  const [projects, setProjects] = useState([]);
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div className={styles.project_container}>
@@ -20,7 +35,16 @@ export default function Projects() {
       </div>
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+            />
+          ))}
       </Container>
     </div>
   );
